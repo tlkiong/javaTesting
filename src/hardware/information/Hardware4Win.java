b1070ab2c8606051b21dacaff045f9696d455e23
gvit00702 <gvit00702@gmail.com>
2015-03-18 18:34:21 +0800

    can get motherboardsn and hddsn for windows only

--- src/hardware/information/Hardware4Win.java
+++ src/hardware/information/Hardware4Win.java
@@ -3,13 +3,13 @@ package hardware.information;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.util.Scanner;
 
 public class Hardware4Win {
+	/**
+	 * Get the motherboard SN
+	 * @return motherboard serial number
+	 */
 	public static String getMotherboardSN() {
 		String result = "";
 		try {
@@ -41,6 +41,11 @@ public class Hardware4Win {
 		return result.trim();
 	}
 
+	/**
+	 * 
+	 * @param drive The home drive - assume to be on C:// 
+	 * @return
+	 */
 	public static String getHardDriveSN(String drive) {
 		String result = "";
 		try {
@@ -69,72 +74,5 @@ public class Hardware4Win {
 			e.printStackTrace();
 		}
 		return result.trim();
-	}
-
-	public static String getBothValue() {
-		String bothSN = "";
-
-		String motherboardSN = getMotherboardSN();
-		String hdSN = getHardDriveSN("C");
-
-		bothSN = motherboardSN + " || " + hdSN;
-
-		return bothSN;
-	}
-
-	private static String sn = null;
-
-	public static final String getSerialNumber() {
-
-		if (sn != null) {
-			return sn;
-		}
-
-		OutputStream os = null;
-		InputStream is = null;
-
-		Runtime runtime = Runtime.getRuntime();
-		Process process = null;
-		try {
-			process = runtime.exec(new String[] { "wmic", "bios", "get",
-					"serialnumber" });
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		os = process.getOutputStream();
-		is = process.getInputStream();
-
-		try {
-			os.close();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		Scanner sc = new Scanner(is);
-		try {
-			while (sc.hasNext()) {
-				String next = sc.next();
-				System.out.println("next: " + next);
-				// if ("SerialNumber".equals(next)) {
-				// System.out.println("sc.next(): "+sc.next());
-				// sn = sc.next().trim();
-				// break;
-				// }
-			}
-		} finally {
-			try {
-				sc.close();
-				is.close();
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		if (sn == null) {
-			throw new RuntimeException("Cannot find computer SN");
-		}
-
-		return sn;
 	}
 }
