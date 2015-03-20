29ac34684167870fa82cd16a83b0b6363ffcb865
kiong kiong <gvit00702@gmail.com>
2015-03-20 17:38:27 +0800

    can get unix's information already

--- src/hardware/information/HardwareInformation.java
+++ src/hardware/information/HardwareInformation.java
@@ -1,7 +1,7 @@
 package hardware.information;
 
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -10,10 +10,16 @@ import mainTest.LZString;
 import mainTest.TestGetHardwareInformation;
 
 public class HardwareInformation {
-	
-	public String getHardwareInformation(){
+
+	/**
+	 * Get Hardware Informations including OS
+	 * 
+	 * @return the hardwareID which is encoded
+	 */
+	public String getHardwareInformation() {
 		String whatOS = OsValidator.whichOs();
-		String hardwareId= "";
+		String hardwareId = "";
+		String encodedHardwareId = "";
 		if (whatOS.equals(OsValidator.OSTYPE.WINDOWS.toString())) {
 			System.out.println("Windows - Your OS is: " + whatOS);
 			hardwareId = getWinInfo();
@@ -26,12 +32,47 @@ public class HardwareInformation {
 		} else {
 			System.out.println("Your OS is: " + whatOS);
 		}
-		
-		String encodedHardwareId = encodedId(30,60,hardwareId);
-		
+
+		if (hardwareId != null && !hardwareId.isEmpty()) {
+			encodedHardwareId = encodedId(0, hardwareId.length(), hardwareId);
+		} else {
+			encodedHardwareId = "";
+		}
 		return encodedHardwareId;
 	}
 	
+	/**
+	 * Get Hardware Informations including OS with specific length
+	 * 
+	 * @param startIndex
+	 * @param endIndex
+	 * @return
+	 */
+	public String getHardwareInformation(int startIndex, int endIndex) {
+		String whatOS = OsValidator.whichOs();
+		String hardwareId = "";
+		String encodedHardwareId = "";
+		if (whatOS.equals(OsValidator.OSTYPE.WINDOWS.toString())) {
+			System.out.println("Windows - Your OS is: " + whatOS);
+			hardwareId = getWinInfo();
+		} else if (whatOS.equals(OsValidator.OSTYPE.MAC.toString())) {
+			System.out.println("Mac - Your OS is: " + whatOS);
+			hardwareId = getMacInfo();
+		} else if (whatOS.equals(OsValidator.OSTYPE.UNIX.toString())) {
+			System.out.println("Unix - Your OS is: " + whatOS);
+			hardwareId = getUnixInfo();
+		} else {
+			System.out.println("Your OS is: " + whatOS);
+		}
+
+		if (hardwareId != null && !hardwareId.isEmpty()) {
+			encodedHardwareId = encodedId(startIndex, endIndex, hardwareId);
+		} else {
+			encodedHardwareId = "";
+		}
+		return encodedHardwareId;
+	}
+
 	/**
 	 * Encode Data using LZString
 	 * 
@@ -46,37 +87,43 @@ public class HardwareInformation {
 		String correctEncodedValue = newEncodedValue.replaceAll("/", "-");
 		return correctEncodedValue;
 	}
-	
+
 	/**
 	 * Encode the Hardware ID
 	 * 
-	 * @param startIndex - Start Index of the substring
-	 * @param endIndex - End Index of the substring
-	 * @param hardwareID - Original Hardware ID
+	 * @param startIndex
+	 *            - Start Index of the substring
+	 * @param endIndex
+	 *            - End Index of the substring
+	 * @param hardwareID
+	 *            - Original Hardware ID
 	 * @return encoded Hardware ID with length of endIndex-startIndex
 	 */
-	public static String encodedId(int startIndex, int endIndex, String hardwareID) {
+	public static String encodedId(int startIndex, int endIndex,
+			String hardwareID) {
 		return encodeData(hardwareID).substring(startIndex, endIndex);
 	}
-	
+
 	/**
 	 * Getting hardware information for windows OS
+	 * 
 	 * @return
 	 */
 	public static String getWinInfo() {
 		String hardwareId = "";
-		URL url = HardwareInformation.class.getClassLoader().getResource("Resources/getHardwareInformation.bat");
+		URL url = HardwareInformation.class.getClassLoader().getResource(
+				"Resources/getHardwareInformation.bat");
 		try {
 			Runtime runtime = Runtime.getRuntime();
-			Process proc = runtime
-					.exec("powershell.exe  \""+url.getPath().toString().substring(1)+"\"  ");
+			Process proc = runtime.exec("powershell.exe  \""
+					+ url.getPath().toString().substring(1) + "\"  ");
 			InputStream is = proc.getInputStream();
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader reader = new BufferedReader(isr);
 			String line = "";
 			while ((line = reader.readLine()) != null) {
 				hardwareId += line + "||";
-				//System.out.println("result: " + result);
+				// System.out.println("hardwareId: " + hardwareId);
 			}
 			reader.close();
 			proc.getOutputStream().close();
@@ -85,11 +132,38 @@ public class HardwareInformation {
 		}
 		return hardwareId.trim();
 	}
-	
 
+	/**
+	 * Getting hardware information for Unix information
+	 * 
+	 * @return
+	 */
 	private String getUnixInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		String hardwareId = "";
+		try {
+			Process proc = Runtime.getRuntime().exec("/bin/bash -c lspci");
+			InputStream is = proc.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader reader = new BufferedReader(isr);
+			String line = "";
+			int index = 0;
+			while ((line = reader.readLine()) != null) {
+				index = line.lastIndexOf("(rev");
+				if (index != -1) {
+					hardwareId += line.substring((line.lastIndexOf(":") + 2),
+							index).trim() + "||";
+				} else {
+					hardwareId += line.substring((line.lastIndexOf(":") + 2)).trim()
+							+ "||";
+				}
+				//System.out.println("hardwareId: " + hardwareId);
+			}
+			reader.close();
+			proc.getOutputStream().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return hardwareId;
 	}
 
 	private String getMacInfo() {
--- src/mainTest/TestGetHardwareInformation.java
+++ src/mainTest/TestGetHardwareInformation.java
@@ -1,12 +1,14 @@
 package mainTest;
+
 import hardware.information.HardwareInformation;
 
 public class TestGetHardwareInformation {
 
 	public static void main(String[] args) {
 		HardwareInformation hardwareInformation = new HardwareInformation();
-		String hardwareId = hardwareInformation.getHardwareInformation();
-		
+		String hardwareId = hardwareInformation.getHardwareInformation(30, 60);
+		//String hardwareId = hardwareInformation.getHardwareInformation();
+
 		System.out.println("Hardware ID: " + hardwareId);
 	}
 }
